f8ad7dcd140987a0e0e9d381dcd9cfcbfa18cdde
Mark <s3778046@student.rmit.edu.au>
2022-03-07 12:32:09 +1100

    Added header.php and footer.php, Modified index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php echo "hello"; ?>
+<?php include ("header.php"); ?>
+    
+
+
+
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+
+<?php include ("header.php"); ?>
