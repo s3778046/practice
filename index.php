6450a72fc41096ecdfe3d77d92d446ea19b947dd
Mark <s3778046@student.rmit.edu.au>
2022-03-07 12:39:49 +1100

    Added Navigation bar to header.php

--- index.php
+++ index.php
@@ -4,18 +4,6 @@
 
 
 
-<div class="container">
-  <div class="row">
-    <div class="col">
-      Column
-    </div>
-    <div class="col">
-      Column
-    </div>
-    <div class="col">
-      Column
-    </div>
-  </div>
-</div>
 
-<?php include ("header.php"); ?>
+
+<?php include ("footer.php"); ?>
